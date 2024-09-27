d7429d61c2950f7a5d449100cfa0da2d2829e18f
Putra Sedana <putrasedana03@gmail.com>
2024-09-27 21:05:28 +0800

    Fix the cors error 3

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ const PORT = process.env.PORT || 8000;
 
 dotenv.config();
 
-app.use(cors());
-
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/api", authRoutes);
 app.use("/api", taskRoutes);
