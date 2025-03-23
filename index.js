afe3c2b34764fd277aca6592e638a02a8d0d0687
Putra Sedana <putrasedana03@gmail.com>
2025-03-23 08:41:02 +0800

    add server.timeout

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+const server = http.createServer(app);
+
+server.timeout = 300000;
 
 const PORT = process.env.PORT || 8000;
 const HOST = process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0";
