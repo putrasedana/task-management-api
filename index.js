e0c63db7ea5599b8d9ad99804ede01e3e62847dc
Putra Sedana <putrasedana03@gmail.com>
2025-03-23 08:28:04 +0800

    edit the package.json scripts

--- index.js
+++ index.js
@@ -8,35 +8,28 @@ import userRoutes from "./routes/user.route.js";
 import path from "path";
 import { fileURLToPath } from "url";
 
-// Load environment variables
 dotenv.config();
 
-// Konversi __dirname di ES Module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// Konfigurasi PORT dan HOST
 const PORT = process.env.PORT || 8000;
 const HOST = process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0";
 
-// Middleware
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-// Routes
 app.use(authRoutes);
 app.use(taskRoutes);
 app.use(userRoutes);
 
-// Default route
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-// Koneksi ke database & Jalankan server
 connectDB()
   .then(() => {
     app.listen(PORT, HOST, () => {
@@ -45,5 +38,5 @@ connectDB()
   })
   .catch((err) => {
     console.error("Database connection failed", err);
-    process.exit(1); // Keluar jika gagal koneksi database
+    process.exit(1);
   });
